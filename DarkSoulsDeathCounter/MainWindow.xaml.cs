acbad6a6ca6aeae9133d85fc149b99595e987bc3
JoshuaMa <majun945@gmail.com>
2017-10-16 23:29:25 +0800

    基本实现功能

--- DarkSoulsDeathCounter/MainWindow.xaml.cs
+++ DarkSoulsDeathCounter/MainWindow.xaml.cs
@@ -1,16 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.IO;
+using System.Text;
+
 
 namespace DarkSoulsDeathCounter
 {
@@ -22,6 +15,52 @@ namespace DarkSoulsDeathCounter
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Drop(object sender, DragEventArgs e)
+        {
+            Tb.Text = "";
+            var array = (Array) e.Data.GetData(DataFormats.FileDrop);
+            if (array == null) return;
+            string fileName = array.GetValue(0).ToString();
+            ReadFile(fileName);
+        }
+
+        private void ReadFile(string fileName)
+        {
+            var byteName = new byte[32];
+            var byteDeath = new byte[4];
+            var info = "";
+            try
+            {
+                var aFile = new FileStream(fileName, FileMode.Open);
+                aFile.Seek(0x3C0, SeekOrigin.Begin);
+                Debug.WriteLine("0x" + aFile.Position.ToString("X"), "POSITION");
+                for (var i = 0; i < 10; i++)
+                {
+                    aFile.Read(byteName, 0, 32);
+                    if (byteName[0] != '\0')
+                    {
+                        aFile.Seek(0x1F008, SeekOrigin.Current);
+                        aFile.Read(byteDeath, 0, 4);
+                        aFile.Seek(-0x1F12C, SeekOrigin.Current);
+                        string playerName = Encoding.Unicode.GetString(byteName);
+                        Debug.WriteLine(playerName.Split('\0')[0], "PLAYER NAME");
+                        info += "玩家姓名: " + playerName.Split('\0')[0] + "\n";
+                        int testDeath = byteDeath[0] + (byteDeath[1] << 8) + (byteDeath[2] << 16) + (byteDeath[3] << 32);
+                        Debug.WriteLine(testDeath, "DEATH COUNT");
+                        info += "死亡次数: " + testDeath + "\n\n";
+                        aFile.Seek(0x100, SeekOrigin.Current);
+                    }
+                    aFile.Seek(0x60020, SeekOrigin.Current);
+                }
+                Tb.Text = info;
+            }
+            catch (IOException e)
+            {
+                Debug.WriteLine(e);
+                throw;
+            }
         }
     }
 }
